a4a660e5788d18e84183c468f73a648e005b76de
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-07-31 22:16:33 +0530

    header edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <div className="header">
       <div className="header--brand">
-        <img className="header--img" src="/src/images/trollFace.png" />
-        <h3 className="header--title">Meme Generator</h3>
+        <img className="header--brand_img" src="/src/images/trollFace.png" />
+        <h3 className="header--brand_title">Meme Generator</h3>
       </div>
       <p className="header--text">React course - Project 3</p>
     </div>
